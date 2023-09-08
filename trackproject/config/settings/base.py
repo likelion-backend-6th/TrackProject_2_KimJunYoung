4ad3aca84c302a19e638865c4c0ecf7c67f22772
JunYoung_Kim <hjklo0220@gmail.com>
2023-09-09 01:45:15 +0900

    [fix] fix admin page

--- trackproject/config/settings/base.py
+++ trackproject/config/settings/base.py
@@ -128,10 +128,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = "/var/www/html/static"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/html/static/",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
