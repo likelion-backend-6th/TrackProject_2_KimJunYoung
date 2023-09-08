366c0c366e8276c53a5bf0db126f7efdc15a030a
JunYoung_Kim <hjklo0220@gmail.com>
2023-09-09 02:42:35 +0900

    [fix] add authorized update post

--- trackproject/blog/views.py
+++ trackproject/blog/views.py
@@ -49,13 +49,9 @@ class PostViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request: Request, *args, **kwargs):
-        user = request.user
-        post: Post = self.get_object()
-        if not post.access_by_post(user):
-            return Response(status=status.HTTP_401_UNAUTHORIZED, data="Unauthorized")
         return super().retrieve(request, *args, **kwargs)
 
-    def destroy(self, request, *args, **kwargs):
+    def destroy(self, request: Request, *args, **kwargs):
         user = request.user
         post: Post = self.get_object()
         if not post.access_by_post(user):
@@ -63,9 +59,12 @@ class PostViewSet(viewsets.ModelViewSet):
 
         return super().destroy(request, *args, **kwargs)
 
-    @extend_schema(deprecated=True)
-    def update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST, data="Deprecated API")
+    def update(self, request: Request, *args, **kwargs):
+        user = request.user
+        post: Post = self.get_object()
+        if not post.access_by_post(user):
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data="Unauthorized")
+        return super().update(request, *args, **kwargs)
 
     @extend_schema(deprecated=True)
     def partial_update(self, request, *args, **kwargs):
