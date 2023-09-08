834fd605557599b0ef7e79322610a4af2f312ba1
JunYoung_Kim <hjklo0220@gmail.com>
2023-09-09 03:13:22 +0900

    [test] add test_permission_post_update_delete_self

--- trackproject/blog/tests.py
+++ trackproject/blog/tests.py
@@ -29,5 +29,34 @@ class PostTest(APITestCase):
         self.assertNotIn(self.authorized_user1, res.data)
 
     # 본인의 게시물만 수정, 삭제가 가능한지 테스트
+    def test_permission_post_update_delete_self(self):
+        self.client.force_login(self.authorized_user1)
+        res: HttpResponse = self.client.post("/blog/post/", self.post_data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        post_id = res.data["id"]
+        self.post_data["title"] = "update title"
+
+        # 다른 사용자일때 수정
+        self.client.force_login(self.authorized_user2)
+        res: HttpResponse = self.client.put(f"/blog/post/{post_id}/", self.post_data)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # 본인일때 수정
+        self.client.force_login(self.authorized_user1)
+        res: HttpResponse = self.client.put(f"/blog/post/{post_id}/", self.post_data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["title"], "update title")
+
+        # 다른 사용자일때 삭제
+        self.client.force_login(self.authorized_user2)
+        res: HttpResponse = self.client.delete(f"/blog/post/{post_id}/")
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # 본인일때 삭제
+        self.client.force_login(self.authorized_user1)
+        res: HttpResponse = self.client.delete(f"/blog/post/{post_id}/")
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
     # follow / unfollow 기능이 잘 작동하는지 테스트
     # follow한 사람들이 올린 게시물을 잘 확인할 수 있는지 테스트
