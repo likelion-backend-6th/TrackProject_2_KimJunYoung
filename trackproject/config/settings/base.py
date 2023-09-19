45157f5821c8898a28aaaed6cc33f8b76dbe0d35
JunYoung_Kim <hjklo0220@gmail.com>
2023-09-19 21:35:17 +0900

    [feat] add HealthCheckMiddleware

--- trackproject/config/settings/base.py
+++ trackproject/config/settings/base.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+VERSION = "0.2.0"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -51,6 +52,7 @@ INSTALLED_APPS += [
 ]
 
 MIDDLEWARE = [
+    "common.middleware.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
